34db222c35fe4774cde77e12c1af981defad87c3
onFire(Abhi) <40654066+AbhiShake1@users.noreply.github.com>
2022-04-27 08:16:45 +0545

    add shops and menus from backend

--- shop/admin.py
+++ shop/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
 
 # Register your models here.
+from shop.models import Shop, Menu
+
+admin.site.register(Shop)
+admin.site.register(Menu)
--- shop/models.py
+++ shop/models.py
@@ -1,3 +1,22 @@
-from django.db import models
+from django.db.models import *
+
 
 # Create your models here.
+class Shop(Model):
+    name = CharField(max_length=200)
+    image = CharField(max_length=1000)
+    address = CharField(max_length=200)
+    city = CharField(max_length=200)
+    slug = CharField(max_length=200)
+
+    def __str__(self):
+        return str(self.name)
+
+
+class Menu(Model):
+    drinks = JSONField()
+    snacks = JSONField()
+    shop = OneToOneField(Shop, on_delete=CASCADE)
+
+    def __str__(self):
+        return str(self.shop.name)
